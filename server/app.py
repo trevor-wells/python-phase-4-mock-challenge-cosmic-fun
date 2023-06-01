08e35464d9a0a84d27413c294aba0ec73880bf29
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-31 20:42:13 -0600

    final commit

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 from models import db, Scientist, Planet, Mission
 
@@ -11,16 +12,97 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
-
 db.init_app(app)
+api = Api(app)
+
+class Index(Resource):
+
+    def get(self):
+        return make_response("<h1>Home Page</h1>", 200)
+
+class Scientists(Resource):
+
+    def get(self):
+        scientists = [{"id": scientist.id, "name": scientist.name, "field_of_study": scientist.field_of_study, "avatar": scientist.avatar} for scientist in Scientist.query.all()]
+        return scientists, 200
+
+    def post(self):
+        try:
+            data = request.get_json()
+            new_scientist = Scientist(
+                name = data["name"],
+                field_of_study = data["field_of_study"],
+                avatar = data["avatar"]
+            )
+            db.session.add(new_scientist)
+            db.session.commit()
+            return new_scientist.to_dict(), 201
+        except ValueError:
+            return {"errors": ["validation errors"]}, 403
+
+class ScientistById(Resource):
+
+    def get(self, id):
+        scientist = Scientist.query.filter(Scientist.id == id).first()
+        if scientist:
+            scientist_dict = scientist.to_dict(rules=('planets',))
+            return scientist_dict, 200
+        return {"error": "Scientist not found"}, 400
+
+    def patch(self, id):
+        data = request.get_json()
+        try:
+            scientist = Scientist.query.filter(Scientist.id == id).first()
+            if not scientist:
+                return {"error": "Scientist not found"}, 404
+            if data['name']:
+                scientist.name = data['name']
+            if data['field_of_study']:
+                scientist.field_of_study = data['field_of_study']
+            if data['avatar']:
+                scientist.avatar = data['avatar']
+            db.session.add(scientist)
+            db.session.commit()
+            return scientist.to_dict(), 202
+        except ValueError:
+            return {"errors": ["validation errors"]}, 403
+
+    def delete(self, id):
+        scientist = Scientist.query.filter(Scientist.id == id).first()
+        if not scientist:
+            return {"error": "Scientist not found"}, 404
+        db.session.delete(scientist)
+        db.session.commit()
+        return {}, 204
 
 
-@app.route('/')
-def index():
-    response = make_response(
-        {"message": "Hello Scientists!"}
-    )
+class Planets(Resource):
 
+    def get(self):
+        planets = [{"id": planet.id, "name": planet.name, "distance_from_earth": planet.distance_from_earth, "nearest_star": planet.nearest_star, "image": planet.image} for planet in Planet.query.all()]
+        return planets, 200
+
+class Missions(Resource):
+
+    def post(self):
+        try:
+            data = request.get_json()
+            new_mission = Mission(
+                name = data["name"],
+                scientist_id = data["scientist_id"],
+                planet_id = data["planet_id"]
+            )
+            db.session.add(new_mission)
+            db.session.commit()
+            return new_mission.planet.to_dict(), 201
+        except ValueError:
+            return {"errors": ["validation errors"]}, 403
+
+api.add_resource(Index, '/')
+api.add_resource(Scientists, '/scientists')
+api.add_resource(ScientistById, '/scientists/<int:id>')
+api.add_resource(Planets, '/planets')
+api.add_resource(Missions, '/missions')
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,26 +4,84 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
-
-
-class Mission(db.Model, SerializerMixin):
-    __tablename__ = 'missions'
-
-    id = db.Column(db.Integer, primary_key=True)
-
+metadata = MetaData(naming_convention = {"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",})
+db = SQLAlchemy(metadata = metadata)
 
 class Scientist(db.Model, SerializerMixin):
     __tablename__ = 'scientists'
 
-    id = db.Column(db.Integer, primary_key=True)
+    serialize_rules = ('-missions.scientist', '-planet.scientists',)
 
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    field_of_study = db.Column(db.String)
+    avatar = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    missions = db.relationship('Mission', back_populates = 'scientist', cascade='all, delete-orphan')
+    planets = association_proxy('missions', 'planet', creator=lambda pl: Mission(planet=pl))
+
+    @validates('name, field_of_study')
+    def validate_scientist(self, key, value):
+        if key == 'name' and not value:
+            raise ValueError("Scientist must have a name.")
+        if key == 'name' and value in [scientist.name for scientist in Scientist.query.all()]:
+            raise ValueError("Scientist's name must be unique.")
+        if key == 'field_of_study' and not value:
+            raise ValueError("Scientist must have a field of study.")
+        return value
+    
+    def __repr__(self):
+        return f'<Scientist {self.id}>'
+        
+class Mission(db.Model, SerializerMixin):
+    __tablename__ = 'missions'
+
+    serialize_rules = ('-scientist.missions', '-planet.missions',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+    scientist = db.relationship('Scientist', back_populates='missions')
+    planet = db.relationship('Planet', back_populates='missions')
+
+    @validates('name, scientist, planet')
+    def validate_mission(self, key, value):
+        if not value:
+            if key == 'name':
+                raise ValueError("Mission must have a name.")
+            elif key == 'scientist':
+                raise ValueError("Mission must have a scientist.")
+            elif key == 'planet':
+                raise ValueError("Mission must have a planet.")
+        if key == 'scientist' and value in self.scientists:
+            raise ValueError("Scientists canot join the same mission twice.")
+        return value
+    
+    def __repr__(self):
+        return f'<Mission {self.id}>'
 
 class Planet(db.Model, SerializerMixin):
     __tablename__ = 'planets'
 
+    serialize_rules = ('-missions.planet', '-scientist.planets')
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    distance_from_earth = db.Column(db.String)
+    nearest_star = db.Column(db.String)
+    image = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    missions = db.relationship('Mission', back_populates = 'planet', cascade='all, delete-orphan')
+    scientists = association_proxy('missions', 'scientist', creator=lambda sc: Mission(scientist=sc))
+
+    def __repr__(self):
+        return f'<Planet {self.id}>'
